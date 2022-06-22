00d2911826d9f7e189a38ebed636c9fdc5cc9fcd
1874467LuyoloMbatha <1874467@students.wits.ac.za>
2022-06-22 22:18:50 +0200

    projectile script

--- Top Down Game/Assets/Script/Projectile/projectileGun.cs
+++ Top Down Game/Assets/Script/Projectile/projectileGun.cs
@@ -14,13 +14,55 @@ public class projectileGun : MonoBehaviour
     //Gun Stats//
     public float timeBeweenSHooting, spread, reloadTime, timeBetweenShots;
     public int magazineSize, bulletsPerTap;
-    public bool allowButtonHold;
+    public bool allowButtonHold; // is the gun automatic. 
 
-    int bulletsLeft, bulletsShot; 
+    int bulletsLeft, bulletsShot;
     //stopped @1:22//
 
-    
-    
+    //Bools//
+    bool shooting, readyToShoot, reloading;
 
-   
+    //Reference// 
+    public Camera fpsCam;
+    public Transform attackPoint;
+
+    //Bug fixes
+    public bool allowInvoke = true;
+
+    private void Awake()
+    {
+        //make sure magazine is full//
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+
+    private void Update()
+    {
+        MyInput();
+        
+    }
+
+    private void MyInput()
+    {
+        //Checks if allowed to hold down button and take corresponding input//
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0); //Tap everytime you need to shoot.
+
+        //Shooting//
+        if (readyToShoot && shooting && bulletsLeft > 0) //2:45
+        {
+            //set bullets shot to 0 
+            bulletsShot = 0; 
+        
+        } 
+
+        
+    
+    }
+
+
+
+
+
+
 }
