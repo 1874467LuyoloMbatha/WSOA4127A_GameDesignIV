3d0d5f6e8a016e82032044b1ead550bb1e84968d
1874467LuyoloMbatha <1874467@students.wits.ac.za>
2022-06-30 16:18:22 +0200

    jump won't work

--- Top Down Game/Assets/Script/FPS/pMovement.cs
+++ Top Down Game/Assets/Script/FPS/pMovement.cs
@@ -7,12 +7,23 @@ public class pMovement : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed;
 
-    public float groundedDrag;  
+    public float groundedDrag;
+
+    //Jumping//
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump; 
+
 
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
     bool grounded;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space; 
+
 
     public Transform orientation;
 
@@ -27,7 +38,6 @@ public class pMovement : MonoBehaviour
     {
         rBody = GetComponent<Rigidbody>();
         rBody.freezeRotation = true; // prevents object from falling over.
-        
     }
 
     private void Update()
@@ -35,26 +45,35 @@ public class pMovement : MonoBehaviour
         //ground check//
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
 
+        MyInput();
+        SpeedControl();
+
         //Handle drag//
         if (grounded)
             rBody.drag = groundedDrag;
         else
             rBody.drag = 0; 
-
-
-        MyInput();
     }
 
     private void FixedUpdate() // Responsible for physics//
     {
         MovePlayer();
-        
     }
 
     private void MyInput() 
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical"); 
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //when to jump//
+        if (Input.GetKey(jumpKey) && readyToJump && grounded) 
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown); 
+        }
     }
 
     private void MovePlayer() 
@@ -62,9 +81,37 @@ public class pMovement : MonoBehaviour
         //calculation movement direction//
         movementDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rBody.AddForce(movementDirection.normalized * moveSpeed * 10f, ForceMode.Force); //mess around with this float variabble theough.
+        //on ground//
+        if(grounded)
+           rBody.AddForce(movementDirection.normalized * moveSpeed * 10f, ForceMode.Force); //mess around with this float variabble theough.
 
-                                                                       
-    
+        //In Air//
+        else if(!grounded)
+            rBody.AddForce(movementDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+
+    private void SpeedControl() 
+    {
+        Vector3 flatVel = new Vector3(rBody.velocity.x, 0f, rBody.velocity.z);
+
+        //limit velocity if needed(Might remove this)//
+        if (flatVel.magnitude > moveSpeed) 
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rBody.velocity = new Vector3(limitedVel.x, rBody.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump() 
+    {
+        //reset y velocity//
+        rBody.velocity = new Vector3(rBody.velocity.x, 0f, rBody.velocity.z);
+
+        rBody.AddForce(transform.up * jumpForce, ForceMode.Impulse);  
+    }
+
+    private void ResetJump() 
+    {
+        readyToJump = true;
     }
 }
